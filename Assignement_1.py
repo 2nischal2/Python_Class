ac2d2294fcba8d05b165e8d46d562fb439a3d38b
2nischal2 <waglenischal72@gmail.com>
2026-02-23 12:17:04 +0545

    First assignement

---
+++ Assignement_1.py
@@ -0,0 +1,23 @@
+#1.Comment
+#This is  a single line comment 
+
+#2.Multi-line comment 
+'''This 
+is 
+muliple 
+line 
+comment'''
+
+#3.Write a program to calculate sum of two number by taking input from user.
+a = int(input("One digit = "))
+b = int(input("Another digit = "))
+sum_int = a + b
+
+print("The sum of given two digits is", sum_int)
+
+#4.Convert the integer value to float value in same value in same program
+sum_float = float(sum_int)
+
+#5.Check the types of each variable data types
+print(type(sum_int))
+print(type(sum_float))
