81df573c0bf67ffa8e807dce1b5e01ec1987782e
2nischal2 <waglenischal72@gmail.com>
2026-02-23 12:23:51 +0545

    Python-lab-exercise

---
+++ Python_Lab_Exercise.py
@@ -0,0 +1,44 @@
+# #=========================================================================================================================================================================
+# # 1. Write a Python program using nested if-else statements to determine a student’s grade based on marks: Marks >= 80: A , Marks >= 60: B,Marks >= 40: C , Marks < 40 : F
+# #=========================================================================================================================================================================
+# '''Soln'''
+# Python = P = 80
+# Calculus = C = 70
+# Digitology = D = 100
+# Socilology = S = 50
+# AI = 80
+# English = E = 90
+
+# Total_marks = P+C+D+S+AI+E
+# Marks = Total_marks / 6
+
+# if P >= 40 and C >= 40 and D >= 40 and S >= 40 and AI >= 40 and E >= 40:
+#     if Marks >= 80:
+#         print("Passed....... Grade : A ")
+#     if 80 > Marks >= 60 :
+#         print("Passed....... Grade :  B")
+#     if 60 > Marks >= 40:
+#         print("Passed....... Grade : C ")
+# else :
+#     print("Failed....")
+
+
+# #========================================================================================================
+# # 2. Write a Python program to find the largest of three numbers entered by the user using if-elif-else.
+# #========================================================================================================
+# '''Soln'''
+# x , y , z = map(int, (input("Enter 3 numbers : ").split()))
+
+# if x > y > z :
+#     print(x,"is the greatest")
+# elif y > x > z:
+#     print(y,"is the greatest")
+# else :
+#     print(z,"is the greatest")
+
+
+#==================================================================================
+# 3. Write a Python program to check whether a number is prime or not using a loop.
+#==================================================================================
+'''Soln'''
+
